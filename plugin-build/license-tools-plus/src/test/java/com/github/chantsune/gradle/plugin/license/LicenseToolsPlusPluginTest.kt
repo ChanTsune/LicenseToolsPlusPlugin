74a1329f024fcfe599c390b210f5657052291134
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-01-30 00:16:20 +0900

    Change project name

---
+++ plugin-build/license-tools-plus/src/test/java/com/github/chantsune/gradle/plugin/license/LicenseToolsPlusPluginTest.kt
@@ -0,0 +1,15 @@
+package com.github.chantsune.gradle.plugin.license
+
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
+
+class LicenseToolsPlusPluginTest {
+
+    @Test
+    fun `plugin is applied correctly to the project`() {
+        val project = ProjectBuilder.builder().build()
+        project.pluginManager.apply("com.github.chantsune.gradle.plugin.license-tools-plus")
+
+        assert(project.tasks.getByName(LicenseToolsPlusTask.NAME) is LicenseToolsPlusTask)
+    }
+}
